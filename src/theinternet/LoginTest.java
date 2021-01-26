ef361592511df6c92e2ab524eed271256eee5850
Aaron Evans <aaron.evans@testery.io>
2021-01-26 10:55:45 -0700

    add projects and cleanup

---
+++ src/theinternet/LoginTest.java
@@ -0,0 +1,61 @@
+package theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.out.println("Testing login with Firefox and Selenium");
+		System.setProperty("webdriver.chrome.driver", "test\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		// open login page (add https -- change to cause a conflict)
+		driver.get("https://the-internet.herokuapp.com/login");
+		
+		// set window size 
+		driver.manage().window().maximize();
+		
+		// enter user name
+		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		
+		// enter password
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+
+		// click login button
+		driver.findElement(By.xpath("//form[@id='login']/button")).click();
+		
+		// verify logged into secure area
+		String message = driver.findElement(By.cssSelector(".flash")).getText();
+		if (message.contains("You logged into a secure area!"))
+		{
+			System.out.println("login succeeded");
+		}
+		else 
+		{
+			throw new RuntimeException("Login failed");
+		}
+		
+		// click logout button
+		driver.findElement(By.linkText("Logout")).click();
+		
+		
+		// verify logout
+		message = driver.findElement(By.className("flash")).getText();
+		if (message.contains("You logged out of the secure area!"))
+		{
+			System.out.println("lougout succeeded");
+		}
+		else 
+		{
+			throw new RuntimeException("Logout failed");
+		}
+				
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
--- test/webdriverbasicsPartII/NavigationCommands.java
+++ test/webdriverbasicsPartII/NavigationCommands.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 //import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
