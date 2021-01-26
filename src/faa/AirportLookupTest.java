bf27065228d97685ce00e67dbd4098bb375e3f47
Aaron Evans <aaron.evans@testery.io>
2021-01-26 11:15:15 -0700

    clean up Airport Lookup Test

--- src/faa/AirportLookupTest.java
+++ src/faa/AirportLookupTest.java
@@ -5,31 +5,44 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-public class AirportLookupTest {
-  private WebDriver driver;
-  JavascriptExecutor js;
-  @BeforeMethod
-public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-  }
-  @AfterMethod
-public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void airportLookup() throws InterruptedException {
-    driver.get("https://www.faa.gov/");
-    driver.manage().window().setSize(new Dimension(1680, 1027));
-    js.executeScript("window.scrollTo(0,0)");
-    driver.findElement(By.id("airportCode")).sendKeys("GPI");
-    driver.findElement(By.id("checkAirport")).click();
-    
-    Thread.sleep(1000);
-    String airportName = driver.findElement(By.cssSelector(".airportInfo b")).getText();
-    System.out.println("airportName: " + airportName);
-  }
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+public class AirportLookupTest
+{
+	private WebDriver driver;
+	private JavascriptExecutor js;
+	private WebDriverWait wait;
+
+	@Test
+	public void airportLookup() throws InterruptedException
+	{
+		driver.get("https://www.faa.gov/");
+		driver.manage().window().maximize();
+		js.executeScript("window.scrollTo(0,0)");
+		driver.findElement(By.id("airportCode")).sendKeys("GPI");
+		driver.findElement(By.id("checkAirport")).click();
+		
+		Thread.sleep(3000);
+		
+		String airportName = driver.findElement(By.cssSelector(".airportInfo b")).getText();	
+		System.out.println("airportName: " + airportName);
+	}
+  
+	@BeforeMethod
+	public void setUp()
+	{
+		driver = new ChromeDriver();
+		js = (JavascriptExecutor) driver;
+		wait = new WebDriverWait(driver, 30);
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
 }
