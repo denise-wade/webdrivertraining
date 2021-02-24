c8e691491e6e81b0b21426d460bd4f20abc4826b
Aaron Evans <aaron.evans@testery.io>
2021-02-24 10:10:23 -0700

    add edge driver example

--- src/SeleniumLogin.java
+++ src/SeleniumLogin.java
@@ -1,27 +1,43 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 
 public class SeleniumLogin
 {
 	public static void main(String[] args) throws InterruptedException
 	{
 		// telling java where to find chromedriver.exe so it can start it up
-		System.setProperty("webdriver.chrome.driver", "	test\\resources\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "test\\resources\\chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver", "test\\resources\\geckodriver.exe");
 		System.setProperty("webdriver.ie.driver", "test\\resources\\IEDriverServer.exe");
+		System.setProperty("webdriver.edge.driver", "test\\resources\\msedgedriver.exe");
 		
 		// start chrome browser
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
 		
 		// or start firefox browser
 //		WebDriver driver = new FirefoxDriver();
 		
 		// or start ie 
 //		WebDriver driver = new InternetExplorerDriver();
+		
+		// pr start edge
+		WebDriver driver = new EdgeDriver();
+		
+		// resize to test responsive layout on iphone
+	    driver.manage().window().setSize(new Dimension(418,896));
+
+	    // set implicit wait to 60 seconds
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
 		
 		// open URL
 		driver.get("https://the-internet.herokuapp.com/login");
@@ -47,6 +63,13 @@ public class SeleniumLogin
 		// wait a bit so we what happened
 		Thread.sleep(3000);
 	    
+		// go back to login page
+		driver.navigate().back();
+		
+		// wait a bit so we what happened
+		Thread.sleep(3000);
+			    
+				
 		/// close browser
 		driver.quit();
 	}
