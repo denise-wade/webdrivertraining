7f7b4577bfe4349046109efa0adb0d92b6afad58
Aaron Evans <aarone@one-shore.com>
2021-02-24 08:45:27 -0700

    Update FirstSeleniumApp.java

--- src/FirstSeleniumApp.java
+++ src/FirstSeleniumApp.java
@@ -5,7 +5,7 @@ public class FirstSeleniumApp
 {
 	public static void main(String[] args)
 	{
-		System.out.println("We're going to start chrome");
+		System.out.println("We've started chrome");
 		
 		// telling java where to find chromedriver.exe so it can start it up
 		System.setProperty("webdriver.chrome.driver", "resources/windows/chromedriver.exe");
