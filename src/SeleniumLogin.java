f3ce719b6eb22016daef61d752be874330045a36
Aaron Evans <aaron.evans@testery.io>
2021-02-24 09:15:26 -0700

    Implement SeleniumLogin

---
+++ src/SeleniumLogin.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class SeleniumLogin
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		// telling java where to find chromedriver.exe so it can start it up
+		System.setProperty("webdriver.chrome.driver", "test\\resources\\chromedriver.exe");
+		
+		// start chrome browser
+		WebDriver chrome = new ChromeDriver();
+		
+		// open URL
+	    chrome.get("https://the-internet.herokuapp.com/login");
+	    
+	    // find username field and type the username
+	    chrome.findElement(By.id("username")).sendKeys("tomsmith");
+	    
+	    // find password field and type the password
+	    chrome.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+
+	    // wait a bit so we what happened
+	    Thread.sleep(3000);
+	    
+	    // find the login button and click it
+	    chrome.findElement(By.className("fa-sign-in")).click();
+		
+	    // wait a bit so we what happened
+	    Thread.sleep(3000);
+	    
+	    // close browser
+		chrome.quit();
+	}
+}
