0440a388a8912f811fd77f003431023793cbce7c
Aaron Evans <aaron.evans@testery.io>
2021-02-24 13:16:22 -0700

    add simple testng test

---
+++ test/SimpleTestNG.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class SimpleTestNG
+{
+
+	@Test
+	public void testHello()
+	{
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void testGoodbye()
+	{
+		System.out.println("Goodbye");
+	}
+}
