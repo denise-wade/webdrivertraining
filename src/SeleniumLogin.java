58a4228292aeb2846a098a4f901cc6698ab797d5
Aaron Evans <aaron.evans@testery.io>
2021-02-24 09:49:33 -0700

    add IE support

--- src/SeleniumLogin.java
+++ src/SeleniumLogin.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.By;
 
 public class SeleniumLogin
@@ -9,14 +10,18 @@ public class SeleniumLogin
 	public static void main(String[] args) throws InterruptedException
 	{
 		// telling java where to find chromedriver.exe so it can start it up
-		System.setProperty("webdriver.chrome.driver", "test\\resources\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "	test\\resources\\chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver", "test\\resources\\geckodriver.exe");
+		System.setProperty("webdriver.ie.driver", "test\\resources\\IEDriverServer.exe");
 		
 		// start chrome browser
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
 		
 		// or start firefox browser
 //		WebDriver driver = new FirefoxDriver();
+		
+		// or start ie 
+		WebDriver driver = new InternetExplorerDriver();
 		
 		// open URL
 		driver.get("https://the-internet.herokuapp.com/login");
