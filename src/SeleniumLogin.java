b75ed10ecb6071fcb947446e0c6ac2d73dec8011
Aaron Evans <aaron.evans@testery.io>
2021-02-24 09:23:03 -0700

    add firefox implementation

--- src/SeleniumLogin.java
+++ src/SeleniumLogin.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
 
 public class SeleniumLogin
@@ -9,29 +10,30 @@ public class SeleniumLogin
 	{
 		// telling java where to find chromedriver.exe so it can start it up
 		System.setProperty("webdriver.chrome.driver", "test\\resources\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "test\\resources\\geckodriver.exe");
 		
 		// start chrome browser
-		WebDriver chrome = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		
 		// open URL
-		chrome.get("https://the-internet.herokuapp.com/login");
+		driver.get("https://the-internet.herokuapp.com/login");
 	    
 		// find username field and type the username
-		chrome.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.id("username")).sendKeys("tomsmith");
 	    
 		// find password field and type the password
-		chrome.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
 
 		// wait a bit so we what happened
 		Thread.sleep(3000);
 	    
 		// find the login button and click it
-		chrome.findElement(By.className("fa-sign-in")).click();
+		driver.findElement(By.className("fa-sign-in")).click();
 		
 		// wait a bit so we what happened
 		Thread.sleep(3000);
 	    
 		/// close browser
-		chrome.quit();
+		driver.quit();
 	}
 }
